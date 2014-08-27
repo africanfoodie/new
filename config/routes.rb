73232a092b74061ae5e957d9fa011e1f236df104
Emma <emma_williams1@hotmail.co.uk>
2014-08-27 11:42:08 +0100

    amended root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/subscription'
 
+  root 'pages#user'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
