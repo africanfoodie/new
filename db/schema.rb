c33e0d8f08cc9538ef5a1f482c5a578348d20d5d
Emma <emma_williams1@hotmail.co.uk>
2014-08-28 11:49:07 +0100

    generated migration for adding user id to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828100529) do
+ActiveRecord::Schema.define(version: 20140828104805) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140828100529) do
     t.integer  "stock"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
