c462b21081a634115631b5ce60e64a8322f02378
Emma <emma_williams1@hotmail.co.uk>
2014-08-28 14:54:07 +0100

    work on editing products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,7 +73,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :price, :stock)
+      params.require(:product).permit(:title, :description, :price, :stock, :image)
     end
 
     def check_user
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
 
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+
+
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
  
