30e95479212ae326cee85213e3b462593a688105
Emma <emma_williams1@hotmail.co.uk>
2014-08-28 11:03:23 +0100

    added additional form fields for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name, :line1, :line2, :town, :county, :postcode
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
 
