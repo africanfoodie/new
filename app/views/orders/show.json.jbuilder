012b526ab7f5c5a4749ce6dce98b59e6d279498e
Emma <emma_williams1@hotmail.co.uk>
2014-08-29 12:05:49 +0100

    created scaffold for order

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :address, :city, :county, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+
   get 'pages/payment'
 
   resources :products
