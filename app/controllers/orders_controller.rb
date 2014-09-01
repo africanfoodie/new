497625ca7416d6e1ab083a1c4de266f744b0a9fb
Emma <emma_williams1@hotmail.co.uk>
2014-09-01 11:51:28 +0100

    some debugging

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
     begin
       charge = Stripe::Charge.create(
         :amount => (@product.price * 100).floor,
-        :currency => "gbp",
+        :currency => "usd",
         :card => token
       )
     flash[:notice] = "Thanks for ordering!"
