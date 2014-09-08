5dbeb3f05029b2c72153e15cb61aa19df9ca0b27
Emma <emma_williams1@hotmail.co.uk>
2014-09-08 11:15:04 +0100

    more carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905142854) do
+ActiveRecord::Schema.define(version: 20140908101414) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20140905142854) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
