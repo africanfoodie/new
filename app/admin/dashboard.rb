6b186cea5b13ccb198bbdbdf7c3ca693dd7f6c36
Emma <emma_williams1@hotmail.co.uk>
2014-09-08 10:55:14 +0100

    comments to the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,6 +10,28 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
+
+
+    
+
+# ActiveAdmin::Dashboards.build do
+#   section "Recent Products" do
+#     table_for Product.order("released_at desc").limit(5) do
+#       column :name do |product|
+#         link_to product.name, [:admin, product]
+#       end
+#       column :released_at
+#     end
+#     strong { link_to "View All Products", admin_products_path }
+#   end
+# end
+
+
+
+
+
+
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
