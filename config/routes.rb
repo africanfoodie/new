2bf273dd98fc0917ab6e8d385aaf644fdb7f0955
Emma <emma_williams1@hotmail.co.uk>
2014-09-08 16:48:24 +0100

    cleaning up a few things

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #tells rails needs product id number
 end
 
-  get 'pages/payment'
+  # get 'pages/payment'
 
   get 'home/about'
 
@@ -30,7 +30,7 @@ end
 
   get 'pages/subscription'
 
-  root 'pages#user'
+  root 'home#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
