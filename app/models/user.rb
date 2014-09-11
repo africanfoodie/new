9c8dcae7ad33d62852bd0befae3b0e4061850bb1
Emma <emma_williams1@hotmail.co.uk>
2014-09-11 16:29:36 +0100

    small commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         validates :name, presence: true
+  validates :name, presence: true
 
   has_many :listings, dependent: :destroy
   # a listings existence depends on the existence of the user that created it
