481ad7d193f0dc16b389c46f7d7364b9394e3c11
Em01 <emma_williams1@hotmail.co.uk>
2014-09-15 11:47:21 +0100

    more contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,9 @@
 class MessagesController < InheritedResources::Base
 	
+	def create
+	if @message.save
+  	flash[:notice] = 'Thanks for Your Message'
+  	format.html { redirect_to root_path }
+  end
+end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,3 @@
 class Message < ActiveRecord::Base
+validates_presence_of :name, :subject, :body
 end
