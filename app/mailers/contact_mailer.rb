ca81613b0751d41d23f51c2965872734d63190f3
Em01 <emma_williams1@hotmail.co.uk>
2014-09-15 15:23:05 +0100

    sorting errors for action mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,7 +7,22 @@ class ContactMailer < ActionMailer::Base
   #   en.contact_mailer.message.subject
   #
   
-    def message(message)
+    # def message(message)
+    # subject    message.subject
+    # body       :message => message
+    # recipients CONTACT_RECIPIENT
+    # from       message.email
+    # sent_on    Time.now
+    # end
+
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.message.subject
+  #
+
+    def deliver_message(message)
     subject    message.subject
     body       :message => message
     recipients CONTACT_RECIPIENT
@@ -16,4 +31,5 @@ class ContactMailer < ActionMailer::Base
     end
 
   end
+
 
