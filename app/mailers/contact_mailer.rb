30b07e586496268a889bb68e33899ca1926f6751
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-18 20:15:59 +0530

    Email fixes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,33 +1,11 @@
 class ContactMailer < ActionMailer::Base
   default from: "amrata.baghel@gmail.com"
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.message.subject
-  #
-  
-    # def message(message)
-    # subject    message.subject
-    # body       :message => message
-    # recipients CONTACT_RECIPIENT
-    # from       message.email
-    # sent_on    Time.now
-    # end
-
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.message.subject
-  #
-
-def deliver_message(user)
-  # @message = message
-  # mail(subject: message.subject, body: message.body, recipients: CONTACT_RECIPIENT, from: message.email, sent_on: Time.now)
- end
-
+  def sample_email(user)
+     mail(to: "sahil1345@gmail.com", subject: "Hey Sample Buddy") do |format|
+      format.text {render :text => "Knock Knock!"}
+    end   
   end
+end
 
 
 
