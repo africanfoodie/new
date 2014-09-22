a94087e763e4215d32c3c2ad342a5c666c89ec56
Em01 <emma_williams1@hotmail.co.uk>
2014-09-22 14:16:54 +0100

    push confict

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,6 @@ module Site
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-
-
-config.middleware.use PDFKit::Middleware, :print_media_type => true
-
     
     
   end
