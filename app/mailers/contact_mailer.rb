b29fae24b4ce2a55321211d18efcacec760a2006
Em01 <emma_williams1@hotmail.co.uk>
2014-09-23 14:26:00 +0100

    more heroku

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
-class ContactMailer < ActionMailer::Base
-  default from: "emtestrails@gmail.com"
-  def sample_email(user)
-     mail(to: user.email, subject: "Hey Sample Buddy") do |format|
-      format.text {render :text => "Knock Knock!"}
-    end   
-  end
-end
+# class ContactMailer < ActionMailer::Base
+#   default from: "emtestrails@gmail.com"
+#   def sample_email(user)
+#      mail(to: user.email, subject: "Hey Sample Buddy") do |format|
+#       format.text {render :text => "Knock Knock!"}
+#     end   
+#   end
+# end
 
 
 
