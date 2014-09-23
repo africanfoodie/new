31cf17c4d93737d02a3589a1790e7e1069f30d06
Em01 <emma_williams1@hotmail.co.uk>
2014-09-23 14:41:27 +0100

    more heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Devise gem, remember to change localhost 3000 to actual application host
-  # config.action_mailer.default_url_options = { :host => 'http://pacific-dusk-5405.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://pacific-dusk-5405.herokuapp.com' }
 
   config.assets.precompile += %w( vendor/modernizr.js )
   
