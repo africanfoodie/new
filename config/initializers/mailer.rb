203f1109e1779fd03cff770d65e61e86892c0f4e
Em01 <emma_williams1@hotmail.co.uk>
2014-09-23 14:43:33 +0100

    heroku removing mail for now

--- config/initializers/mailer.rb
+++
@@ -1,9 +0,0 @@
-# ActionMailer::Base.smtp_settings = {
-#   :address => 'smtp.gmail.com',
-#   :port => 587,
-#   :domain => "localhost:3000",
-#   :user_name => ENV["EMAIL"], #this should come from applications.yml ENV['user_name']
-#   :password => ENV["PASSWORD"], #this should come from applications.yml ENV['user_name']
-#   :authentication => "plain",
-#   :enable_starttls_auto => true
-# }
